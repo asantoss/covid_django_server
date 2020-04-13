7416e280c3f3eb19ab7c2a6d0103f9ed166657bc
Alexander Santos <alexsantosantana@live.com>
2020-04-12 21:39:54 -0400

    Settingup for heroku

--- rest_api/data_sources/worlometer/daily_scraper.py
+++ rest_api/data_sources/worlometer/daily_scraper.py
@@ -1,11 +1,14 @@
 from scraper import Scraper
+import os
+
+base_url = os.getenv('APP_URL')
 
 states = Scraper('https://www.worldometers.info/coronavirus/country/us/',
                  'usa_table_countries_today', 'states-data',
-                 'http://127.0.0.1:8000/api/states/')
+                 base_url + 'states/')
 states.daily_scrape()
 
 countries = Scraper('https://www.worldometers.info/coronavirus/',
                     'main_table_countries_today', 'countries-data',
-                    'http://127.0.0.1:8000/api/countries/')
+                    base_url + 'countries/')
 countries.daily_scrape()
